a9ef514bab0db8d931a0b529c44f958350729adf
Chunngai <mikumikku3939@icloud.com>
2020-11-30 16:47:09 +0800

    ens

--- ens.sh
+++ ens.sh
@@ -3,16 +3,20 @@
 set -e
 set -x
 
+OUT_EXT=".out.tmp"
+
 TXTS=`ls *txt`
 for f in $TXTS
 do
 	dos2unix $f
 
-	OUT="$f.out"
+	OUT="$f$OUT_EXT"
 	echo "$f" > $OUT
 	echo -e "\n" >> $OUT
 	awk -F $'\t' '{print $2}' $f >> $OUT
 done
 
-OUTS=`ls *.out | sort -n`
+OUTS=`ls *$OUT_EXT | sort -n`
 paste $OUTS > ens_out
+
+rm *$OUT_EXT
